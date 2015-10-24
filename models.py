48d471574e2962896a21aa28ec355d70247f7423
ChrisFreeman <chris.freeman.pdx@gmail.com>
2015-10-24 16:00:56 -0700

    Add minimum Models to all test execution
    Fix ImportError: cannot import name 'User'

---
+++ models.py
@@ -0,0 +1,23 @@
+#
+'''Tacocat Models'''
+
+from peewee import *
+
+class User(Model):
+    """User Model
+    """
+    
+    def __init__(self):
+        """Initialize User Model
+        """
+        pass
+
+class Taco(Model):
+    """Taco Model
+    """
+    
+    def __init__(self, ):
+        """Initialize Taco Model
+        """
+        
+        pass
